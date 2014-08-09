c0c5d649a38671695871101438d542e1745de8f8
Rezel <xxfugtiivexx@hotmail.com>
2014-08-09 19:40:07 -0400

    Failed upload

--- src/main/java/com/thedreamsanctuary/chatscape/commands/GroupList.java
+++ src/main/java/com/thedreamsanctuary/chatscape/commands/GroupList.java
@@ -6,27 +6,31 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.thedreamsanctuary.chatscape.executors.addGroup;
-import com.thedreamsanctuary.chatscape.executors.gInfo;
-import com.thedreamsanctuary.chatscape.executors.remGroup;
-import com.thedreamsanctuary.chatscape.executors.toggleGroupList;
+import com.thedreamsanctuary.chatscape.executors.AddGroup;
+import com.thedreamsanctuary.chatscape.executors.GList;
+import com.thedreamsanctuary.chatscape.executors.GetGroups;
+import com.thedreamsanctuary.chatscape.executors.Pinfo;
+import com.thedreamsanctuary.chatscape.executors.RemGroup;
+import com.thedreamsanctuary.chatscape.executors.ToggleGroupList;
 import com.thedreamsanctuary.chatscape.data.Data;
 import com.thedreamsanctuary.chatscape.data.Info;
+import com.thedreamsanctuary.chatscape.data.MessageWriter;
 
 public class GroupList implements CommandExecutor
 {
-	addGroup ag = new addGroup();
-	remGroup rg = new remGroup();
-	gInfo gi = new gInfo();
-	toggleGroupList tgl = new toggleGroupList();
+	AddGroup ag = new AddGroup();
+	RemGroup rg = new RemGroup();
+	GList gl = new GList();
+	GetGroups gg = new GetGroups();
+	ToggleGroupList tgl = new ToggleGroupList();
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String str,String[] args)
 	{
 		if (sender instanceof Player)
 		{
+			Info i = Data.getInfo((Player) sender);
 			if (args.length >= 1)
 			{
-				Info i = Data.getInfo((Player) sender);
 				if (args[0].equalsIgnoreCase("add"))
 				{
 					ag.executor(i,args);
@@ -37,11 +41,23 @@ public class GroupList implements CommandExecutor
 				}
 				else if (args[0].equalsIgnoreCase("info"))
 				{
-					gi.executor(i, args);
+					MessageWriter.modeStatus(i);
 				}
 				else if (args[0].equalsIgnoreCase("toggle"))
 				{
 					tgl.executor(i, args);
+				}
+				else if (args[0].equalsIgnoreCase("mylist"))
+				{
+					gl.executor(i, args);
+				}
+				else if (args[0].equalsIgnoreCase("listgroups"))
+				{
+					gg.executor(i, args);
+				}
+				else
+				{
+					i.getPlayer().sendMessage(ChatColor.DARK_RED + args[0] + ChatColor.DARK_AQUA + " is not a valid arguement");
 				}
 			}
 			else
@@ -49,10 +65,13 @@ public class GroupList implements CommandExecutor
 				sender.sendMessage(ChatColor.DARK_AQUA + "add" + ChatColor.BLUE + "- Adds a group to your group list");
 				sender.sendMessage(ChatColor.DARK_AQUA + "rem" + ChatColor.BLUE + "- Removes a group from your group list");
 				sender.sendMessage(ChatColor.DARK_AQUA + "toggle" + ChatColor.BLUE + "- Toggles group list on or off");
-				sender.sendMessage(ChatColor.DARK_AQUA + "info" + ChatColor.BLUE + "- Lists all groups you currently have added");
+				sender.sendMessage(ChatColor.DARK_AQUA + "info" + ChatColor.BLUE + "- gets your info.....");
+				sender.sendMessage(ChatColor.DARK_AQUA + "listgroups" + ChatColor.BLUE + "- Lists groups you have access to");
+				sender.sendMessage(ChatColor.DARK_AQUA + "list" + ChatColor.BLUE + "- \n"
+						+ ChatColor.DARK_AQUA +"/gl list " + ChatColor.WHITE + "-" + ChatColor.BLUE +" Returns all users on your group list \n"
+						+ ChatColor.DARK_AQUA +"/gl list <string> " + ChatColor.WHITE + "-" + ChatColor.BLUE + "searches for whole names or partial matches");
 			}
-			
-		}
+		}	
 		return false;
 	}
 }
--- src/main/java/com/thedreamsanctuary/chatscape/commands/WhisperList.java
+++ src/main/java/com/thedreamsanctuary/chatscape/commands/WhisperList.java
@@ -6,24 +6,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.thedreamsanctuary.chatscape.executors.addPlayer;
-import com.thedreamsanctuary.chatscape.executors.override;
-import com.thedreamsanctuary.chatscape.executors.pInfo;
-import com.thedreamsanctuary.chatscape.executors.remPlayer;
-import com.thedreamsanctuary.chatscape.executors.toggleWhisperList;
 import com.thedreamsanctuary.chatscape.data.Data;
 import com.thedreamsanctuary.chatscape.data.Info;
+import com.thedreamsanctuary.chatscape.data.MessageWriter;
+import com.thedreamsanctuary.chatscape.executors.AddPlayer;
+import com.thedreamsanctuary.chatscape.executors.Override;
+import com.thedreamsanctuary.chatscape.executors.Pinfo;
+import com.thedreamsanctuary.chatscape.executors.RemPlayer;
+import com.thedreamsanctuary.chatscape.executors.SetWLLimit;
+import com.thedreamsanctuary.chatscape.executors.ToggleGroupList;
+import com.thedreamsanctuary.chatscape.executors.ToggleWhisperList;
+import com.thedreamsanctuary.chatscape.executors.WList;
 
 public class WhisperList implements CommandExecutor
 {
 	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) 
 	{
 		Info i = Data.getInfo((Player) sender);
-		addPlayer ap = new addPlayer();
-		remPlayer rp = new remPlayer();
-		pInfo pi = new pInfo();
-		override ovr = new override();
-		toggleWhisperList twl = new toggleWhisperList();		
+		AddPlayer ap = new AddPlayer();
+		RemPlayer rp = new RemPlayer();
+		Override ovr = new Override();
+		ToggleWhisperList twl = new ToggleWhisperList();
+		SetWLLimit swl = new SetWLLimit();
+		WList wl = new WList();
+
 		
 		System.out.println("THE PLAYER" + i.getPlayer());
 		if (args.length >= 1)
@@ -38,7 +44,7 @@ public class WhisperList implements CommandExecutor
 			}
 			else if (args[0].equalsIgnoreCase("info"))
 			{
-				pi.executor(i, args);
+				MessageWriter.modeStatus(i);
 			}
 			else if (args[0].equalsIgnoreCase("toggle"))
 			{
@@ -48,6 +54,14 @@ public class WhisperList implements CommandExecutor
 			{
 				ovr.executor(i, args);
 			}
+			else if (args[0].equalsIgnoreCase("limit"))
+			{
+				swl.executor(i, args);
+			}
+			else if (args[0].equalsIgnoreCase("list"))
+			{
+				wl.executor(i, args);
+			}
 		}
 		else
 		{
@@ -55,6 +69,10 @@ public class WhisperList implements CommandExecutor
 			sender.sendMessage(ChatColor.DARK_AQUA + "rem" + ChatColor.BLUE + "- Removes a player from your group list");
 			sender.sendMessage(ChatColor.DARK_AQUA + "toggle" + ChatColor.BLUE + "- Toggles player list on or off");
 			sender.sendMessage(ChatColor.DARK_AQUA + "info" + ChatColor.BLUE + "- Lists all players you currently have added");
+			sender.sendMessage(ChatColor.DARK_AQUA + "limit" + ChatColor.BLUE + "- Gets or sets the value of the limit");
+			sender.sendMessage(ChatColor.DARK_AQUA + "list" + ChatColor.BLUE + "- \n"
+					+ ChatColor.DARK_AQUA +"/wl list " + ChatColor.WHITE + "-" + ChatColor.BLUE +" Returns all users on your whisper list \n"
+					+ ChatColor.DARK_AQUA +"/wl list <string> " + ChatColor.WHITE + "-" + ChatColor.BLUE + "searches for whole names or partial matches");
 		}
 		return false;
 	}
